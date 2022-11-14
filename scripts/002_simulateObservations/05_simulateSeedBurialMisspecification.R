43db0da37507709d2124cd6bd3ac007b872f4c88
gregor-fausto <fausto.siegmund@gmail.com>
2022-11-13 18:31:03 -0800

    expand range of germination to simulate; fix number of replicates

--- scripts/002_simulateObservations/05_simulateSeedBurialMisspecification.R
+++ scripts/002_simulateObservations/05_simulateSeedBurialMisspecification.R
@@ -15,7 +15,7 @@ outDataDirectory <- paste0(primaryDirectory,"outputs/001_simulateObservations/03
 # - Parameters for simulation ----
 
 # - +Probability of germination----
-p.g <- c(.1)
+p.g <- c(.1,.2,.3,.4,.5)
 
 # - +Probability of mortality in first year----
 p.m <- c(.1)
@@ -35,28 +35,27 @@ parameterTable=rbind(parameterTable)
 n=dim(parameterTable)[1]
 
 # - + Set number of replicate simulated datasets ----
-n.replicate = 10
+n.replicate = 250
 
 # - +for each row of the parameter table ----
 for(h in 1:n){
-  
+
   tmp <- parameterTable[h,]
-  
+
   # - +extract parameters ----
   p.g <- tmp$p.g;p.m <- tmp$p.m;n.start <- tmp$n.start;n.bags <- tmp$n.bags
-  
+
   # - +modify mortality so that it increases by 0.1 each year ----
   p.m = c(p.m,p.m+.1,p.m+.2)
-  
+
   # - +set time steps to 1, 2, 3 years ----
   t <- 1:3
-  
+
   # - +for each parameter set, create n.replicate simulated datasets ----
   for(j in 1:n.replicate){
-    
+
     # - +source script for simulation ----
     source(paste0(scriptDirectory,"04_functionSimulateSeedBurialMisspecification.R"))
-    
+
   }
 }
-
